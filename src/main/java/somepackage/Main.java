36c685b2b690ec7a08a71f568e84dd4d695d774b
hanterd <pidjakoff@yandex.ru>
2020-10-15 17:15:33 +0400

    Тест

--- src/main/java/somepackage/Main.java
+++ src/main/java/somepackage/Main.java
@@ -1,10 +1,27 @@
 package somepackage;
 
+
+import somethinggreen.Contract;
+
 public class Main {
     public static void main(String[] args) {
         Some some = new Some("Тестовый");
         System.out.printf(some.getSecret());
-        some.noMoreSecrets = "Тест";
-        System.out.printf(some.noMoreSecrets );
+        System.out.println();
+
+        Other.saySomthing();
+        System.out.println();
+
+        ContractImpl contractImpl = new ContractImpl();
+        contractImpl.soTellMeAboutInterfaces();
+
+
+    }
+    public static class ContractImpl implements Contract {
+
+        @Override
+        public int count() {
+            return 0;
+        }
     }
 }
